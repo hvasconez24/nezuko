c6636ce2c2ced2e915da4c86526d4599821c9ef9
Hilary V <hv101@scarletmail.rutgers.edu>
2022-04-09 16:00:32 -0500

    Update conection.js to Heroku config
    Update conection.js to Heroku config

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection
-const sequelize = new Sequelize('blog', 'root', '0811', {
-  host: 'localhost',
+const sequelize = new Sequelize('heroku_95115ad6de88dcd', 'b313796f5e26a2', 'c4def49e', {
+  host: 'us-cdbr-east-05.cleardb.net',
   dialect: 'mysql',
   port: 3306
 });
