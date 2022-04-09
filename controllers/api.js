7d89f06d55ef63cf61fd88ce75be1ab2341320b4
Hilary V <92323804+hvasconez24@users.noreply.github.com>
2022-04-09 18:05:19 -0500

    changes

--- controllers/api.js
+++ controllers/api.js
@@ -1,10 +1,12 @@
 const express = require('express');
 var router = express.Router();
 
+///connect  api-routes
 const userRoutes = require('./api/user');
 const postRoutes = require('./api/post');
 const commentRoutes = require('./api/comment');
 
+//using routes 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
